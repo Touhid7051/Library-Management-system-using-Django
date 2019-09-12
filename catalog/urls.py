72f30986b82bffcb7c92edad7f62441216066d16
Touhid <touhidulislamtawhid@gmail.com>
2019-09-13 05:45:51 +0600

    First commit

---
+++ catalog/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+
+from . import views
+from django.urls import path
+
+urlpatterns = [path('', views.index, name='index'),
+               path('books/', views.BookListView.as_view(), name='books'),
+               path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+
+               ]
---
+++ catalog/views.py
@@ -0,0 +1,50 @@
+from django.http import Http404
+from django.shortcuts import render
+from django.views import generic, View
+
+from .models import Book, Author, BookInstance, Genre
+from django.contrib.auth.mixins import LoginRequiredMixin
+def index(request):
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()
+
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+    return render(
+        request,
+        'index.html',
+        context={'num_books': num_books, 'num_instances': num_instances,
+                 'num_instances_available': num_instances_available, 'num_authors':
+                  num_authors,'num_visits':num_visits}, # num_visits appended
+                            )
+
+
+class BookListView(generic.ListView):
+    model = Book
+    context_object_name = 'my_book_list' # your own name for the list as a templatevariable
+    #queryset = Book.objects.filter(title__icontains='hello')[:5] # Get 5 books containingthe title hello
+    template_name = 'books/book_list.html' # Specify your owntemplate name/location
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+    def book_detail_view(request, pk):
+        try:
+            book_id = Book.objects.get(pk=pk)
+        except Book.DoesNotExist:
+            raise Http404("Book does not exist")
+        # book_id=get_object_or_404(Book, pk=pk)
+
+        return render(
+            request,
+            'catalog/book_detail.html',
+            context={'book': book_id, }
+            )
+
+class MyView(LoginRequiredMixin, View):
+    login_url = '/login/'
+    redirect_field_name = 'redirect_to'
